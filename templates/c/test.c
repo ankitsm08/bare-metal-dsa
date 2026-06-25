e0f9febc0476fbea39f34ce56528444014383483
Ankit Sharma <ankitsm08@gmail.com>
2026-06-25 21:26:50 +0530

    format(scaffold): better visual separation and formatting

--- templates/c/test.c
+++ templates/c/test.c
@@ -3,13 +3,13 @@
 
 #define RUN_TEST(test_func)                                                                        \
   do {                                                                                             \
-    printf("[RUNNING] %s...\n", #test_func);                                                       \
+    printf("[ RUNNING ] %s...\n", #test_func);                                                     \
     test_func();                                                                                   \
-    printf("[PASS] %s\n", #test_func);                                                             \
+    printf("[ PASSED  ] %s\n\n", #test_func);                                                      \
   } while (0)
 
 int main(void) {
-  printf("Testing __NAME__...\n");
+  printf("Testing __NAME__...\n\n");
 
   printf("All tests passed for __NAME__\n");
   return 0;
--- templates/cpp/test.cpp
+++ templates/cpp/test.cpp
@@ -3,13 +3,13 @@
 
 #define RUN_TEST(test_func)                                                                        \
   do {                                                                                             \
-    std::cout << "[RUNNING] " << #test_func << "...\n";                                            \
+    std::cout << "[ RUNNING ] " << #test_func << "...\n";                                          \
     test_func();                                                                                   \
-    std::cout << "[PASS] " << #test_func << "\n";                                                  \
+    std::cout << "[ PASSED  ] " << #test_func << "\n\n";                                           \
   } while (0)
 
 int main() {
-  std::cout << "Testing __NAME__...\n";
+  std::cout << "Testing __NAME__...\n\n";
 
   std::cout << "All tests passed for __NAME__\n";
   return 0;
